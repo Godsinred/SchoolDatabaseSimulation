11474d4325748624f80d67e947b04aaac8e81de9
Jonathan Ishii <you@example.comjtishii@yahoo.com>
2019-04-17 19:35:40 -0700

    Added index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+
+<html> <head>
+<title>First PHP Example</title> </head>
+<body>
+<?php echo "<p><h1>Output</h1>";
+echo "<h2>Output</h2>";
+echo "<h3>Output</h3></p>"; ?>
+<script language="PHP">
+echo "\n<b>More PHP Output</b><br />\n";
+</script>
+</body> </html>
