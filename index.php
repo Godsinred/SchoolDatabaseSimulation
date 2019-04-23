d382737f6f9a9f896cc564dc66063dbd464e5d57
Jonathan_Ishii <jtishii@yahoo.com>
2019-04-22 20:02:48 -0700

    Made the ability to make queries to the server

--- index.php
+++ index.php
@@ -1,7 +1,36 @@
 <html> <head>
 <title>First PHP Example</title> </head>
 <body>
-  <?php echo "<h1>TESTNIG FROM index.php</h1>"; ?>
+  <?php echo "<h1>TESTNIG FROM index.php</h1>";
+
+  // connects to the database and makes a link
+  $link = mysql_connect('ecs.fullerton.edu', 'cs332t19', 'au0ieLah');
+
+  // checks to see if the connection was made successfully. if not it closes
+  if (!$link)
+  {
+    die('Could not connect: '.mysql_error());
+  }
+  echo "console.log(Connected successfully);";
+
+  // selects the db and checks to make sure it was successfully connected
+  if(!mysql_select_db("cs332t19",$link))
+  {
+    die("Unable to select database \'cs332t19\'");
+  }
+
+  // test query from our db
+  $SQL = "SELECT * FROM test";
+  $result = mysql_query($SQL, $link);
+
+  for($i=0; $i<mysql_numrows($result); $i++)
+  {
+    echo "SSN: ", mysql_result($result, $i, "testCol"), “<br>”;
+  }
+
+  mysql_close($link);
+
+  ?>
 <script language="PHP">
 echo "\n<b>More PHP Output</b><br />\n";
 </script>
