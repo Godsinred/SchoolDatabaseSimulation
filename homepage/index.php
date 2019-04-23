46f77c248d466418597c08a64872ee9eae26ce03
Jonathan Ishii <you@example.comjtishii@yahoo.com>
2019-04-23 01:24:16 -0700

    made the sql thing work and cleaned up some of the homepage files

--- homepage/index.php
+++ homepage/index.php
@@ -4,26 +4,40 @@
   <?php echo "<h1>TESTNIG FROM index.php</h1>";
 
   // connects to the database and makes a link
-  $link = mysql_connect('shell.ecs.fullerton.edu', 'cs332t19', 'au0ieLah');
+  $link = mysql_connect('ecs.fullerton.edu', 'cs332t19', 'au0ieLah');
 
   // checks to see if the connection was made successfully. if not it closes
   if (!$link)
   {
-    die('Could not connect: '.mysql_error());
+    die('Could not connect to shell.ecs.fullerton.edu');
   }
-  echo "console.log(Connected successfully);";
+    echo "<div>console.log(Connected to server successfully);</div>";
 
   // selects the db and checks to make sure it was successfully connected
   if(!mysql_select_db("cs332t19",$link))
   {
     die("Unable to select database \'cs332t19\'");
   }
+  echo "<p>console.log(Connected to database successfully);</p>";
 
+  // basic query for testing
+  $query = "SELECT * from test";
+  // reaches out to the db and makes the query and returns the result in $result
+  $result = mysql_query($query, $link);
 
+  // while there are arrays to be read print them
+  while($row = mysql_fetch_array($result)) {
+      echo $row["testCol"]."<br>";
+  }
+
+  // closes the link between the variable and the server
   mysql_close($link);
 
   ?>
-<script language="PHP">
+
+<!-- <script language="PHP">
 echo "\n<b>More PHP Output</b><br />\n";
-</script>
-</body> </html>
+</script> -->
+
+</body>
+</html>
