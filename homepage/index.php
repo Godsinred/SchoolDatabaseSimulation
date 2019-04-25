156ca30c3d6d873806ce57eb829861066881d1ee
Jonathan Ishii <you@example.comjtishii@yahoo.com>
2019-04-25 13:01:41 -0700

    Added the table to the query so it prints out nicely

--- homepage/index.php
+++ homepage/index.php
@@ -1,112 +1,209 @@
-<html> <head>
-<title>First PHP Example</title> </head>
-<body>
-  <?php echo "<h1>TESTING FROM index.php</h1>";
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 
-  // connects to the database and makes a link
-  $link = mysql_connect('ecs.fullerton.edu', 'cs332t19', 'au0ieLah');
+<html>
+<head>
+  <title>First PHP Example</title> </head>
+	<script src="assets/helper-functions.js"></script>
+	<link rel="stylesheet" href="assets/styles.css">
+</head>
 
-  // checks to see if the connection was made successfully. if not it closes
-  if (!$link)
-  {
-    die('Could not connect to shell.ecs.fullerton.edu');
-  }
-  echo "<div>console.log(Connected to server successfully);</div>";
+	<body>
+		<script src="assets/helper-functions.js"></script>
 
-  // selects the db and checks to make sure it was successfully connected
-  if(!mysql_select_db("cs332t19",$link))
-  {
-    die("Unable to select database \'cs332t19\'");
-  }
-  echo "<p>console.log(Connected to database successfully);</p>";
+		<center>
+			<div id="aqua1">
 
-  // ===== PROFESSOR A =====
-  // Given the social security number of a professor, list the titles, classrooms, meeting
-  // days and time of his/her classes.
-  /*
-  int inputSSN;       // this holds the professor's SSN that was received from the user
+	<div id="aqua2">
+		<div id="aqua3"> </div>
 
-  SELECT PTitle, Classroom, MeetingDays, BeginTime, EndTime
-  FROM Sections S, MeetingDays MD, Professor P
-  WHERE S.ProfSSN = inputSSN AND
-        S.CourseNum = MD.CourseNumber AND
-        S.SNumber = MD.SectionNumber AND
-        S.ProfSSN = P.SSN;
-  */
+	<div class="aqua4">
+		<ul>
+			<h1>Queries</h1>
+			<BR><BR><BR><BR><BR><BR>
+			<p align="left">
+				<!-- Drop down list that can be selected for the user type -->
+				<select id="userType" onchange="updateSearchType()">
+				  <option value="Professor">Professor</option>
+				  <option value="Student">Student</option>
+				</select>
 
-  // ===== PROFESSOR B =====
-  // Given a course number and a section number, count how many students get each
-  // distinct grade, i.e. 'A', 'A-', 'B+', 'B', 'B-', etc.
-  /*
-  int inputCNum;      // this holds the inputted course number from the user
-  int inputSNum;      // this holds the inputted section number from the user
+				<!-- Drop down list that can be selected for the search type -->
+				<select id="searchType" onchange="updateFormOptions()">
+				  <option value="questionA">Social Security Number</option>
+				  <option value="questionB">Course Number</option>
+				</select>
 
-  SELECT Grade, COUNT(*)
-  FROM EnrollmentRecord ER
-  WHERE ER.SectionNum = inputSNum AND
-        ER.CNum = inputCNum
-  GROUP BY Grade;
-  */
+				<!-- Search form -->
+				<form id="searchForm" action="http://ecs.fullerton.edu/~cs332t19/index.php" method = "get">
+					<input type="submit" value="Search">
+				  <text id="search1Text">Social Security Number</text><input id="searchField1" type="text" name="search1"><br>
+					<text id="search2Text"></text>
+				</form>
 
-  // ===== STUDENT A =====
-  // Given a course number list the sections of the course, including the classrooms, the
-  // meeting days and time, and the number of students enrolled in each section.
-  /*
-  int inputCNum;      // this holds the inputted course number from the user
+			</p>
+		</ul>
+	</div>
 
-  SELECT CTitle, Classroom, MeetingDays, BeginTime, EndTime, COUNT(*)
-  FROM Sections S, Course C, MeetingDays MD, EnrollmentRecord ER
-  WHERE S.CourseNum = inputCNum AND
-        S.CourseNum = Course.CNumber AND
-        MD.CourseNumber = S.CourseNum AND
-        MD.SectionNumber = S.SNumber AND
-        ER.SectionNum = S.CourseNum AND
-        ER.CNum = S.SNumber
-  GROUP BY S.SNumber;
-  */
+	<div id="aqua7">DATABASES AND FILES</div>
 
-  // ===== STUDENT B =====
-  // Given the campus wide ID of a student, list all the courses the student took and
-  // the grades.
-  /*
-  int inputCWID;      // this holds the inputted CWID from the user\
+	<div id="aqua5">
+		<h2>Search Results</h2>
+		  <?php
 
-  SELECT C.CTitle, ER.Grade
-  FROM Course C, EnrollmentRecord ER
-  WHERE ER.StudCWID = inputCWID AND
-        ER.Cnum = C.CNumber;
-  */
+      // connects to the database and makes a link
+      $link = mysql_connect('ecs.fullerton.edu', 'cs332t19', 'au0ieLah');
 
-  // basic query for testing
-  // $query = "SELECT * from Professor";
+      // checks to see if the connection was made successfully. if not it closes
+      if (!$link)
+      {
+        die('Could not connect to shell.ecs.fullerton.edu');
+      }
+      echo "<div>console.log(Connected to server successfully);</div>";
 
-  // makes a query based on the daat given
-  $query = "SELECT * from Professor WHERE Professor.SSN=" . htmlspecialchars($_GET["search1"]);
-  echo $query."<br><br>";
+      // selects the db and checks to make sure it was successfully connected
+      if(!mysql_select_db("cs332t19",$link))
+      {
+        die("Unable to select database \'cs332t19\'");
+      }
+      echo "<p>console.log(Connected to database successfully);</p>";
 
-  // reaches out to the db and makes the query and returns the result in $result
-  $result = mysql_query($query, $link);
+      // ===== PROFESSOR A =====
+      // Given the social security number of a professor, list the titles, classrooms, meeting
+      // days and time of his/her classes.
+      /*
+      int inputSSN;       // this holds the professor's SSN that was received from the user
 
-  // while there are arrays to be read print them
-  echo "SSN      PName      PAddress          PTeleNum   Sex   PTitle    Salary   <br>";
-  while($row = mysql_fetch_array($result)) {
-      echo $row["SSN"].$row["PName"].$row["PAddress"].$row["PTeleNum"].$row["Sex"].$row["PTitle"].$row["Salary"]."<br>";
-  }
-  echo "<br><br>";
+      SELECT PTitle, Classroom, MeetingDays, BeginTime, EndTime
+      FROM Sections S, MeetingDays MD, Professor P
+      WHERE S.ProfSSN = inputSSN AND
+            S.CourseNum = MD.CourseNumber AND
+            S.SNumber = MD.SectionNumber AND
+            S.ProfSSN = P.SSN;
+      */
 
-  // a test to output data from the form to the php scriting site
-  echo $_GET["search1"];
-  echo "<br><br>";
-  echo $_GET["search2"];
+      // ===== PROFESSOR B =====
+      // Given a course number and a section number, count how many students get each
+      // distinct grade, i.e. 'A', 'A-', 'B+', 'B', 'B-', etc.
+      /*
+      int inputCNum;      // this holds the inputted course number from the user
+      int inputSNum;      // this holds the inputted section number from the user
 
-  // closes the link between the variable and the server
-  mysql_close($link);
+      SELECT Grade, COUNT(*)
+      FROM EnrollmentRecord ER
+      WHERE ER.SectionNum = inputSNum AND
+            ER.CNum = inputCNum
+      GROUP BY Grade;
+      */
 
-  ?>
+      // ===== STUDENT A =====
+      // Given a course number list the sections of the course, including the classrooms, the
+      // meeting days and time, and the number of students enrolled in each section.
+      /*
+      int inputCNum;      // this holds the inputted course number from the user
 
-<!-- <script language="PHP">
-echo "\n<b>More PHP Output</b><br />\n";
-</script> -->
+      SELECT CTitle, Classroom, MeetingDays, BeginTime, EndTime, COUNT(*)
+      FROM Sections S, Course C, MeetingDays MD, EnrollmentRecord ER
+      WHERE S.CourseNum = inputCNum AND
+            S.CourseNum = Course.CNumber AND
+            MD.CourseNumber = S.CourseNum AND
+            MD.SectionNumber = S.SNumber AND
+            ER.SectionNum = S.CourseNum AND
+            ER.CNum = S.SNumber
+      GROUP BY S.SNumber;
+      */
+
+      // ===== STUDENT B =====
+      // Given the campus wide ID of a student, list all the courses the student took and
+      // the grades.
+      /*
+      int inputCWID;      // this holds the inputted CWID from the user\
+
+      SELECT C.CTitle, ER.Grade
+      FROM Course C, EnrollmentRecord ER
+      WHERE ER.StudCWID = inputCWID AND
+            ER.Cnum = C.CNumber;
+      */
+
+      // basic query for testing
+      // $query = "SELECT * from Professor";
+
+      // makes a query based on the daat given
+      $query = "SELECT * from Professor WHERE Professor.SSN=" . htmlspecialchars($_GET["search1"]);
+      echo $query."<br><br>";
+
+      // reaches out to the db and makes the query and returns the result in $result
+      $result = mysql_query($query, $link);
+
+      // while there are arrays to be read print them
+      echo "
+      <table>
+        <tr>
+          <th>SSN</th>
+          <th>PName</th>
+          <th>PAddress</th>
+          <th>PTeleNum</th>
+          <th>Sex</th>
+          <th>PTitle</th>
+          <th>Salary</th>
+        </tr>
+      ";
+
+      while($row = mysql_fetch_array($result))
+      {
+        echo "<tr>";
+        echo
+        ("<td>"
+        .$row["SSN"]
+        ."</td>"
+
+        ."<td>"
+        .$row["PName"]
+        ."</td>"
+
+        ."<td>"
+        .$row["PAddress"]
+        ."</td>"
+
+        ."<td>"
+        .$row["PTeleNum"]
+        ."</td>"
+
+        ."<td>"
+        .$row["Sex"]
+        ."</td>"
+
+        ."<td>"
+        .$row["PTitle"]
+        ."</td>"
+
+        ."<td>"
+        .$row["Salary"]
+        ."</td>");
+
+        echo "</tr>";
+      }
+
+      echo "</table>";
+      echo "<br><br>";
+
+      // a test to output data from the form to the php scriting site
+      // echo $_GET["search1"];
+      // echo "<br><br>";
+      // echo $_GET["search2"];
+
+      // closes the link between the variable and the server
+      mysql_close($link);
+
+      ?>
+	</div>
+	<div id="aqua6">
+		<div class="center">
+			<a href="http://autumnsunsets.com" target="_blank">Autumn*Sunsets</a>
+		© All Rights Reserved . 2009 . [E.V.3]
+		</div>
+	</div>
+</div>
+</center>
 
 </body>
 </html>
