2862c5b250658521ebfdf57d4bc488a73afebc21
Jonathan Ishii <you@example.comjtishii@yahoo.com>
2019-05-01 01:38:04 -0700

    Fixed all the query possibilities but the student and course number. unable to get that one working.

--- homepage/index.php
+++ homepage/index.php
@@ -3,6 +3,8 @@
 <html>
 <head>
 	<title>School Database Simmulation</title> </head>
+
+	<!-- Links to external style and helper functions -->
 	<script src="assets/helper-functions.js"></script>
 	<link rel="stylesheet" href="assets/styles.css">
 </head>
@@ -16,6 +18,7 @@
 
 	<div id="Main">
 	<header>Results</header>
+		<!-- use the php tag to integrate php into your code -->
     <?php
 
     // connects to the database and makes a link
@@ -26,34 +29,24 @@
     {
       die('Could not connect to shell.ecs.fullerton.edu');
     }
-    echo "<div>console.log(Connected to server successfully);</div>";
 
     // selects the db and checks to make sure it was successfully connected
     if(!mysql_select_db("cs332t19",$link))
     {
       die("Unable to select database \'cs332t19\'");
     }
-    echo "<p>console.log(Connected to database successfully);</p>";
-
-    // a test to output data from the form to the php scriting site
-    // echo $_GET["search1"];
-    // echo "<br><br>";
-    // echo $_GET["search2"];
-  	// echo "<br><br>";
-		// echo $_GET["userType"];
-    // echo "<br><br>";
-    // echo $_GET["searchType"];
 
     // makes a query based on the daat given and prepopulate the table headers
+		// use $_GET[<VALUE NAME>] to get GET request from the browser like a dictionary
     $query = "";
     $tableHeaders = "";
     $dataLoop = array();
     if ($_GET["userType"] == "Professor" && $_GET["searchType"] == "questionA")
     {
-      $query = "SELECT PTitle, Classroom, MeetingDays, BeginTime, EndTime
+      $query = "SELECT PName, PTitle, Classroom, MeetingDays, BeginTime, EndTime
                 FROM Sections S, MeetingDays MD, Professor P
-                WHERE S.ProfSSN = " + $_GET["search1"] +
-                " AND S.CourseNum = MD.CourseNumber AND
+                WHERE S.ProfSSN = ".$_GET["search1"].
+								" AND S.CourseNum = MD.CourseNumber AND
                 S.SNumber = MD.SectionNumber AND
                 S.ProfSSN = P.SSN";
 
@@ -73,8 +66,8 @@
     {
       $query = "SELECT Grade, COUNT(*) as total
                 FROM EnrollmentRecord ER
-                WHERE ER.SectionNum = " + $_GET["search1"] + " AND
-                  ER.CNum = " + $_GET["search2"] +
+                WHERE  ER.CNum= ".$_GET["search1"]." AND
+                   		ER.SectionNum = ".$_GET["search2"].
                 " GROUP BY Grade";
 
       $tableHeaders = "<table id ='OutputTable'>
@@ -89,13 +82,14 @@
     {
       $query = "SELECT CTitle, Classroom, MeetingDays, BeginTime, EndTime, COUNT(*) as total
                 FROM Sections S, Course C, MeetingDays MD, EnrollmentRecord ER
-                WHERE S.CourseNum = " + $_GET["search1"] + " AND
-                  S.CourseNum = Course.CNumber AND
+                WHERE S.CourseNum = ".$_GET["search1"]." AND
+                  S.CourseNum = C.CNumber AND
                   MD.CourseNumber = S.CourseNum AND
                   MD.SectionNumber = S.SNumber AND
-                  ER.SectionNum = S.CourseNum AND
-                  ER.CNum = S.SNumber
-                GROUP BY S.SNumber";
+                  EXISTS (SELECT *
+										FROM EnrollmentRecord, Sections
+										WHERE SectionNum = CourseNum AND
+                  	CNum = SNumber)";
 
       $tableHeaders = "<table id ='OutputTable'>
                         <tr>
@@ -113,8 +107,8 @@
     {
       $query = "SELECT C.CTitle, ER.Grade
                 FROM Course C, EnrollmentRecord ER
-                WHERE ER.StudCWID = " + $_GET["search1"] + " AND
-                  ER.Cnum = C.CNumber";
+                WHERE ER.StudCWID = ".$_GET["search1"]." AND
+                  ER.CNum = C.CNumber";
 
       $tableHeaders = "<table id ='OutputTable'>
                         <tr>
@@ -122,66 +116,10 @@
                           <th>Grade</th>
                         </tr>";
 
-      $dataLoop = array("CTitle", "Classroom", "MeetingDays", "BeginTime", "EndTime", "total");
+      $dataLoop = array("CTitle", "Grade");
     }
 
-    // ===== PROFESSOR A =====
-    // Given the social security number of a professor, list the titles, classrooms, meeting
-    // days and time of his/her classes.
-    /*
-    int inputSSN;       // this holds the professor's SSN that was received from the user
-
-    SELECT PTitle, Classroom, MeetingDays, BeginTime, EndTime
-    FROM Sections S, MeetingDays MD, Professor P
-    WHERE S.ProfSSN = inputSSN AND
-          S.CourseNum = MD.CourseNumber AND
-          S.SNumber = MD.SectionNumber AND
-          S.ProfSSN = P.SSN;
-    */
-
-    // ===== PROFESSOR B =====
-    // Given a course number and a section number, count how many students get each
-    // distinct grade, i.e. 'A', 'A-', 'B+', 'B', 'B-', etc.
-    /*
-    int inputCNum;      // this holds the inputted course number from the user
-    int inputSNum;      // this holds the inputted section number from the user
-
-    SELECT Grade, COUNT(*)
-    FROM EnrollmentRecord ER
-    WHERE ER.SectionNum = inputSNum AND
-          ER.CNum = inputCNum
-    GROUP BY Grade;
-    */
-
-    // ===== STUDENT A =====
-    // Given a course number list the sections of the course, including the classrooms, the
-    // meeting days and time, and the number of students enrolled in each section.
-    /*
-    int inputCNum;      // this holds the inputted course number from the user
-
-    SELECT CTitle, Classroom, MeetingDays, BeginTime, EndTime, COUNT(*)
-    FROM Sections S, Course C, MeetingDays MD, EnrollmentRecord ER
-    WHERE S.CourseNum = inputCNum AND
-          S.CourseNum = Course.CNumber AND
-          MD.CourseNumber = S.CourseNum AND
-          MD.SectionNumber = S.SNumber AND
-          ER.SectionNum = S.CourseNum AND
-          ER.CNum = S.SNumber
-    GROUP BY S.SNumber;
-    */
-
-    // ===== STUDENT B =====
-    // Given the campus wide ID of a student, list all the courses the student took and
-    // the grades.
-    /*
-    int inputCWID;      // this holds the inputted CWID from the user\
-
-    SELECT C.CTitle, ER.Grade
-    FROM Course C, EnrollmentRecord ER
-    WHERE ER.StudCWID = inputCWID AND
-          ER.Cnum = C.CNumber;
-    */
-
+		// temp echo to make sure that the right query is being sent
     echo $query."<br><br>";
 
     // reaches out to the db and makes the query and returns the result in $result
@@ -198,7 +136,7 @@
       // prints out all the items based on the predefined items
       for ($i = 0; $i < count($dataLoop); ++$i)
       {
-        echo "<td>".$row[$dataLoop]."</td>";
+        echo "<td>".$row[$dataLoop[$i]]."</td>";
       }
 
       echo "</tr>";
@@ -235,10 +173,12 @@
   <form id="searchForm" action="http://ecs.fullerton.edu/~cs332t19/index.php" method="GET">
     <input type="submit" value="Search">
     <br><br>
+		<!-- Serach fields for the user to enter in their data -->
     <text id="search1Text">Social Security Number</text><input id="searchField1" type="text" name="search1"><br>
     <text id="search2Text"></text>
+		<!-- these are my hacky way of sending more data into the GET request -->
     <input id="searchFieldHidden1" type="hidden" name="userType" value="Professor"><br>
-    <input id="searchFieldHidden2" type="hidden" name="searchType" value="Social Security Number"><br>
+    <input id="searchFieldHidden2" type="hidden" name="searchType" value="questionA"><br>
   </form>
 
 	</div>
